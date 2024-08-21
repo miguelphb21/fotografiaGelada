b62b32e49fbea8698f09844eef5853b1341e16e8
miguelphb21 <miguel.s.silva9@aluno.senai.br>
2024-08-21 10:48:51 -0300

    Adicao modal

---
+++ stilized-page/modal.js
@@ -0,0 +1,9 @@
+const modal = document.querySelector('.modal--login')
+
+function openModal(){
+    modal.classList.add('active')
+};
+
+function closeModal(){
+    modal.classList.remove('active')
+};
