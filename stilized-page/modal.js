3e522e7fe5461d9b6f8f61ba86956c1d6783e182
miguelphb21 <miguel.s.silva9@aluno.senai.br>
2024-08-22 01:14:18 -0300

    Adicionando window-modal

--- stilized-page/modal.js
+++ stilized-page/modal.js
@@ -1,4 +1,9 @@
-const modal = document.querySelector('.modal--login')
+const windowInformation = document.querySelector('.window-information-photo')
+const modal = document.querySelector('.modal--login');
+const loginUser = document.querySelector('.login');
+const loginPassword = document.getElementById('input-password');
+const userError = document.getElementById('user-error')
+const passwordError = document.getElementById('password-error')
 
 function openModal(){
     modal.classList.add('active')
@@ -7,3 +12,18 @@ function openModal(){
 function closeModal(){
     modal.classList.remove('active')
 };
+
+function openWindow(){
+    windowInformation.classList.add('active')
+};
+
+function closeWindow(){
+    windowInformation.classList.remove('active')
+};
+
+function loginConfirm(){
+    if (loginUser.length === 0 && loginPassword.length === 0 ){
+        loginUser.classList.add('error-login')
+        userError.innerText = "Você não digitou nada"
+    }
+}
