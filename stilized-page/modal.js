cfd12f0b153f53bc9d4d7c99ca246682948ce0e7
miguelphb21 <miguel.s.silva9@aluno.senai.br>
2024-08-23 10:54:40 -0300

    Deixando o modal do logine e das imagens fixo

--- stilized-page/modal.js
+++ stilized-page/modal.js
@@ -1,28 +1,25 @@
 const windowInformation = document.querySelector('.window-information-photo')
 const modal = document.querySelector('.modal--login');
-const loginUser = document.querySelector('.login');
-const loginPassword = document.getElementById('input-password');
-const userError = document.getElementById('user-error')
-const passwordError = document.getElementById('password-error')
+const body = document.querySelector('body')
+// const loginUser = document.querySelector('#input-login');
+// const loginPassword = document.getElementById('input-password');
+// const userError = document.getElementById('user-error');
+// const passwordError = document.getElementById('password-error');
 
 function openModal(){
     modal.classList.add('active')
+    body.classList.add('active')
 };
 
 function closeModal(){
     modal.classList.remove('active')
+    body.classList.remove('active')
 };
-
 function openWindow(){
     windowInformation.classList.add('active')
+    body.classList.add('active')
 };
-
 function closeWindow(){
     windowInformation.classList.remove('active')
+    body.classList.remove('active')
 };
-
-function loginConfirm(){
-    if (loginUser.length === 0 && loginPassword.length === 0 ){
-        userError.innerText = "Você não digitou nada"
-    }
-}
